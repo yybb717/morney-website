f547a29f12f39a4a00a1d469cab3177ecce01412
yybb717 <2638143715@qq.com>
2020-03-24 13:35:13 +0800

    deploy

---
+++ precache-manifest.50d5ec608080be6ae451370fac789cd1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5df4e4ff489f4652c254",
+    "url": "/morney-website/css/app.09b12937.css"
+  },
+  {
+    "revision": "295a81de1c418f1c4dba17aba99a4490",
+    "url": "/morney-website/index.html"
+  },
+  {
+    "revision": "5df4e4ff489f4652c254",
+    "url": "/morney-website/js/app.4c9ba0cc.js"
+  },
+  {
+    "revision": "26d90c46c9c741287205",
+    "url": "/morney-website/js/chunk-vendors.279888fd.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/morney-website/manifest.json"
+  },
+  {
+    "revision": "b2c26cc6af6c1af63ebcef20eab64919",
+    "url": "/morney-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-website/robots.txt"
+  }
+]);
